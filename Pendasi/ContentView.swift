9675e653263169eef42cfc6057ff98a97784be0e
Christina <christina.vp12@gmail.com>
2023-06-12 16:47:17 +0700

    Ubah navigation dan contentview

--- Pendasi/ContentView.swift
+++ Pendasi/ContentView.swift
@@ -12,62 +12,70 @@ struct ContentView: View {
             ZStack {
                 Color("Abu")
                     .ignoresSafeArea()
-                VStack(spacing: 0.0) {
-                    VStack(spacing: 20.0) {
+                VStack {
+                    VStack {
                         Image("OnBoarding")
                             .resizable()
                             .frame(width: 326.0, height: 314.0)
-                        Text("Petualangan kuliner sehat dan lezat untuk si kecil dengan MPASI Mingguan")
-                            .font(.callout)
-                            .fontWeight(.regular)
-                            .multilineTextAlignment(.center)
-                            .foregroundColor(Color.black)
-                        TextField("Nama", text: $name)
-                            .textFieldStyle(RoundedBorderTextFieldStyle())
-                            .padding()
-                        Button(action: {
-                            isDatePickerVisible = true
-                        }) {
-                            TextField("Tanggal Lahir", text: .constant(""), onEditingChanged: { _ in
-                                isDatePickerVisible = true
-                            })
-                            .textFieldStyle(RoundedBorderTextFieldStyle())
-                            .padding()
-                            .multilineTextAlignment(.leading)
-                            .disabled(true)
-                        }
-                        if isDatePickerVisible {
-                            DatePicker(
-                                selection: $birthdate,
-                                displayedComponents: [.date]
-                            ) {
-                                Text("")
+                            .padding(.top, 24)
+                        VStack {
+                            VStack{
+                                Text("PROFILE ANAK")
+                                    .font(.largeTitle)
+                                    .fontWeight(.bold)
+                                    .multilineTextAlignment(.center)
+                                    .foregroundColor(Color.black)
+                                TextField("Nama", text: $name)
+                                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                                    //   .padding()
+                                Button(action: {
+                                    isDatePickerVisible = true
+                                }) {
+                                    TextField("Tanggal Lahir", text: .constant(""), onEditingChanged: { _ in
+                                        isDatePickerVisible = true
+                                    })
+                                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                                   // .padding()
+                                    .multilineTextAlignment(.leading)
+                                    .disabled(true)
+                                }
+                            }
+                            .frame(maxWidth: .infinity)
+                            
+                            Spacer()
+                            
+                            if isDatePickerVisible {
+                                DatePicker(
+                                    selection: $birthdate,
+                                    displayedComponents: [.date]
+                                ) {
+                                    Text("")
+                                    
+                                }
+                                .datePickerStyle(WheelDatePickerStyle())
+                                .padding()
                                 
                             }
-                            .datePickerStyle(WheelDatePickerStyle())
-                            .padding()
-                            
-                        }
-                        NavigationLink(destination: TabVIew(), isActive: $isPlanStarted) {
-                            EmptyView()
-                        }
-                        .navigationBarBackButtonHidden(true)
-                        Button {
-                            isPlanStarted = true
-                        } label: {
-                            Text("Mulai Rencana")
-                                .fontWeight(.bold)
-                                .multilineTextAlignment(.center)
-                                .lineLimit(1)
-                                .padding(.trailing, 100)
-                                .padding(.leading, 94)
-                                .padding(.top, 16)
-                                .padding(.bottom, 16 )
-                                .font(.title2)
-                                .background(Color("AccentColor"))
-                                .cornerRadius(8)
-                                .foregroundColor(.white)
-                                .shadow(radius: 5)
+                            NavigationLink(destination: TabVIew(), isActive: $isPlanStarted) {
+                                EmptyView()
+                            }
+                            .navigationBarBackButtonHidden(true)
+                            Button {
+                                isPlanStarted = true
+                            } label: {
+                                Text("Mulai Rencana")
+                                    .fontWeight(.bold)
+                                   // .multilineTextAlignment(.center)
+                                    .lineLimit(1)
+                                    .frame(maxWidth: .infinity)
+                                    .padding(.top, 16)
+                                    .padding(.bottom, 16 )
+                                    .font(.title2)
+                                    .background(Color("AccentColor"))
+                                    .cornerRadius(8)
+                                    .foregroundColor(.white)
+                                    .shadow(radius: 5)
+                            }
                         }
                     }
                 }
@@ -75,6 +83,10 @@ struct ContentView: View {
                 .toolbarBackground(Color.white, for: .navigationBar)
                 .toolbarBackground(.visible, for: .navigationBar)
             }
+            
+            .padding(.all, 16.0)
+            .background(Color("Abu"))
+            
             .navigationBarItems(trailing: Button(action: {
                 isSkipped = true
             }) {
--- Pendasi/DetailRiwayat.swift
+++ Pendasi/DetailRiwayat.swift
@@ -8,119 +8,127 @@
 import SwiftUI
 
 struct DetailRiwayat: View {
+    @Environment(\.presentationMode) var presentationMode
     var body: some View {
-        NavigationStack {
+      //  NavigationStack {
             ZStack {
                 Color("Abu")
                     .ignoresSafeArea()
                 ScrollView(.vertical, showsIndicators: false) {
-                        VStack{
-                            Group {
-                                Group {
-                                    HStack {
-                                        Text ("23")
-                                            .font(.caption)
-                                            .fontWeight(.bold)
-                                        Text ("MARET")
-                                            .font(.caption)
-                                            .fontWeight(.bold)
-                                        Text ("23")
-                                            .font(.caption)
-                                            .fontWeight(.bold)
-                                        Text (":")
-                                            .font(.caption)
-                                            .fontWeight(.bold)
-                                        Text ("SARAPAN")
-                                            .font(.caption)
-                                            .fontWeight(.bold)
-                                    }
-                                }
-                                .padding(.trailing, 190.0)
-                                Divider()
-                                Group {
-                                    HStack {
-                                        Text("Serat")
-                                            .font(.body)
-                                            .fontWeight(.regular)
-                                        Spacer()
-                                        Spacer()
-                                        Spacer()
-                                        Text("Bayam")
-                                            .font(.body)
-                                            .fontWeight(.regular)
-                                            .foregroundColor(Color("Abu1"))
-                                    }
-                                    .frame(width: 360, height: 20)
-                                    Divider()
-                                    HStack {
-                                        Text("Protein Nabati")
-                                            .font(.body)
-                                            .fontWeight(.regular)
-                                        Spacer()
-                                        Spacer()
-                                        Spacer()
-                                        Text("Tempe")
-                                            .font(.body)
-                                            .fontWeight(.regular)
-                                            .foregroundColor(Color("Abu1"))
-                                    }
-                                    .frame(width: 360, height: 20)
-                                    Divider()
-                                    HStack {
-                                        Text("Lemak")
-                                            .font(.body)
-                                            .fontWeight(.regular)
-                                        Spacer()
-                                        Spacer()
-                                        Spacer()
-                                        Text("Minyak Zaitun")
-                                            .font(.body)
-                                            .fontWeight(.regular)
-                                            .foregroundColor(Color("Abu1"))
-                                    }
-                                    .frame(width: 360, height: 20)
-                                    Divider()
-                                    HStack {
-                                        Text("Reaksi Anak")
-                                            .font(.body)
-                                            .fontWeight(.regular)
-                                        Spacer()
-                                        Spacer()
-                                        Spacer()
-                                        Text("Tidak Alergi dan Suka")
-                                            .font(.body)
-                                            .fontWeight(.regular)
-                                            .foregroundColor(Color("Abu1"))
-                                    }
-                                    .frame(width: 360, height: 20)
-                                    Divider()
-                                    HStack {
-                                        Text("Serat")
-                                            .font(.body)
-                                            .fontWeight(.regular)
-                                        Spacer()
-                                        Spacer()
-                                        Spacer()
-                                        Text("Bayam")
-                                            .font(.body)
-                                            .fontWeight(.regular)
-                                            .foregroundColor(Color("Abu1"))
-                                    }
-                                    .frame(width: 360, height: 20)
-                                    Divider()
-                                }
+                    VStack{
+                        Text ("23 MARET 23 : SARAPAN")
+                            .font(.caption)
+                            .fontWeight(.bold)
+                            .padding(.trailing, 190.0)
+                        Divider()
+                        Group {
+                            HStack {
+                                Text("Serat")
+                                    .font(.body)
+                                    .fontWeight(.regular)
+                                Spacer()
+                                Spacer()
+                                Spacer()
+                                Text("Bayam")
+                                    .font(.body)
+                                    .fontWeight(.regular)
+                                    .foregroundColor(Color("Abu1"))
                             }
+                            .frame(width: 360, height: 20)
                             Divider()
+                            HStack {
+                                Text("Protein Nabati")
+                                    .font(.body)
+                                    .fontWeight(.regular)
+                                Spacer()
+                                Spacer()
+                                Spacer()
+                                Text("Tempe")
+                                    .font(.body)
+                                    .fontWeight(.regular)
+                                    .foregroundColor(Color("Abu1"))
+                            }
+                            .frame(width: 360, height: 20)
+                            Divider()
+                            HStack {
+                                Text("Lemak")
+                                    .font(.body)
+                                    .fontWeight(.regular)
+                                Spacer()
+                                Spacer()
+                                Spacer()
+                                Text("Minyak Zaitun")
+                                    .font(.body)
+                                    .fontWeight(.regular)
+                                    .foregroundColor(Color("Abu1"))
+                            }
+                            .frame(width: 360, height: 20)
+                            Divider()
+                            HStack {
+                                Text("Reaksi Anak")
+                                    .font(.body)
+                                    .fontWeight(.regular)
+                                Spacer()
+                                Spacer()
+                                Spacer()
+                                Text("Tidak Alergi dan Suka")
+                                    .font(.body)
+                                    .fontWeight(.regular)
+                                    .foregroundColor(Color("Abu1"))
+                            }
+                            .frame(width: 360, height: 20)
+                            Divider()
+                            HStack {
+                                Text("Serat")
+                                    .font(.body)
+                                    .fontWeight(.regular)
+                                Spacer()
+                                Spacer()
+                                Spacer()
+                                Text("Bayam")
+                                    .font(.body)
+                                    .fontWeight(.regular)
+                                    .foregroundColor(Color("Abu1"))
+                            }
+                            .frame(width: 360, height: 20)
+                            Divider()
+                        }
+                   
                     }
-                    .padding(.all, 8.0)
+                   
+                
                 }
             }
-            .toolbarBackground(Color.white, for: .navigationBar)
-            .toolbarBackground(.visible, for: .navigationBar)
-            .navigationTitle("Riwayat MPASI")
-            .navigationBarTitleDisplayMode(.inline)
-        }
+         .padding(.all, 16.0)
+         .background(Color("Abu"))
+            
+//            .navigationBarTitle(Text ("Daging + Kentang"), displayMode: .inline)
+            
+            //  .toolbarBackground(Color.white, for: .navigationBar)
+            // .toolbarBackground(.visible, for: .navigationBar)
+            //            .navigationBarTitle(Text ("Daging + Kentang"), displayMode: .inline)
+            // .navigationTitle("Riwayat MPASI")
+            //.navigationBarTitleDisplayMode(.inline)
+       // }
+        .navigationBarTitle(Text ("Daging + Kentang"), displayMode: .inline)
+        .navigationBarBackButtonHidden(true)
+        .navigationBarItems(leading: backButton)
+       
     }
+    
+    
+    var backButton: some View {
+            Button(action:  {
+                presentationMode.wrappedValue.dismiss()
+                // Handle back button action here
+            }) {
+                HStack {
+                    Image(systemName: "arrow.left")
+                    Text("Kembali")
+                }
+            }
+        }
+    
 }
 
 struct DetailRiwayat_Previews: PreviewProvider {
